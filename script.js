65e0c957e101a403e6ac39083d0d3c73663e07f7
Liyajoseph <>
2021-06-02 15:31:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById('img-changer').addEventListener('click', image)
+document.getElementById('text-changer').addEventListener('click', text)
+document.getElementById('hide').addEventListener('click', hide)
+document.getElementById('show').addEventListener('click', show)
+document.getElementById('background').addEventListener('click', bg)
+
+function image () {
+  document.getElementById('picture').src = "images/sleeping.jpg"
+}
+
+function text () {
+  document.getElementById('text1').innerHTML = "Good Evening!"
+}
+
+function hide () {
+  document.getElementById('text1').style.display = "none"
+}
+
+function show () {
+  document.getElementById('text1').style.display = "block"
+}
+
+function bg () {
+  document.body.style.backgroundColor = "violet"
+}
